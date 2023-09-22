0e86dc8f401d87e61e3b2d058f9a4e0f3558a04b
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 16:37:39 +0500

    Exercise 20

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let mountains = ["Mount Everest", "K2", "Nanga Parbat", "Mount Fuji"];
+for (let mountain of mountains) {
+    console.log(mountain);
+}
